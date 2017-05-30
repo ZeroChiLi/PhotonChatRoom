5a3aa57827e9e90937f4ceb2ad436bab55b24c41
ChiLi_Lin <904981663@163.com>
2017-05-30 19:15:34 +0800

    Send Message And Check Response

--- Assets/Scripts/PhotonTest.cs
+++ Assets/Scripts/PhotonTest.cs
@@ -1,17 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using ExitGames.Client.Photon;
-using System;
+using System.Collections.Generic;
 
-public class PhotonTest : MonoBehaviour,IPhotonPeerListener
+public class PhotonTest : MonoBehaviour, IPhotonPeerListener
 {
     PhotonPeer peer;
 
     void Start()
     {
         peer = new PhotonPeer(this, ConnectionProtocol.Udp);
-        peer.Connect("127.0.0.1:5055","Lite");
+        peer.Connect("127.0.0.1:5055", "Lite");
     }
 
     void Update()
@@ -19,27 +17,43 @@ public class PhotonTest : MonoBehaviour,IPhotonPeerListener
         peer.Service();
     }
 
+    void SendMessage()
+    {
+        Dictionary<byte, object> para = new Dictionary<byte, object>();
+        para[255] = "1";
+        peer.OpCustom(255, para, true);
+
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 100, 100, 30), "Set Game ID"))
+        {
+            SendMessage();
+        }
+    }
+
     public void DebugReturn(DebugLevel level, string message)
     {
-        throw new NotImplementedException();
+        Debug.Log(message);
     }
 
     public void OnEvent(EventData eventData)
     {
-        throw new NotImplementedException();
+        Debug.Log("OnEvent() Fucking Called : " + eventData.ToStringFull());
     }
 
     public void OnOperationResponse(OperationResponse operationResponse)
     {
-        throw new NotImplementedException();
+        Debug.Log("Server Reply You,Mother Fucker : " + operationResponse.ToStringFull());
     }
 
     public void OnStatusChanged(StatusCode statusCode)
     {
-        switch(statusCode)
+        switch (statusCode)
         {
             case StatusCode.Connect:
-                Debug.Log("Connect~~~~");
+                Debug.Log("Connect~~~~ ");
                 break;
             case StatusCode.Disconnect:
                 Debug.Log("Disconnect~~~~~");
